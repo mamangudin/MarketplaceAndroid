f86e077a2c503c885100a622cb32c137511c6d8f
ARIEF86 <ajjarief@gmail.com>
2022-01-29 10:58:08 +0700

    init

---
+++ app/src/main/java/com/mamang/marketplace/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mamang.marketplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
